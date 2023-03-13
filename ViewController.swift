de1197bd3fc80dfd87269b7c85d61b8b0d868bd5
vivektarun <vivektarun1234@gmail.com>
2023-03-13 19:08:23 +0530

    starting file is done

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Vivek Tarun on 13/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
